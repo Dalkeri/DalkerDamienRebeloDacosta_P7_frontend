37613f4b1168463bfc825a24c184fa23c0446af1
Dalkeri <6567636+Dalkeri@users.noreply.github.com>
2021-10-22 00:05:53 +0200

    add comments

--- src/components/Thread.vue
+++ src/components/Thread.vue
@@ -1,29 +1,45 @@
 <template>
-  <div class="Thread">
-    <h4>==================================================</h4>
-    <div v-if="modifyThread">
-      <addThread :threadDatas="threadDatas" @my-event="updateThreads" />
-    </div>
-    <div v-else>
-      <h3>{{ threadDatas.title }}</h3>
-      <p>{{ threadDatas.content }}</p>
-      <!-- <p>par: {{ threadDatas.User.firstName + " " + threadDatas.User.lastName}}</p> -->
-    </div>
-    <div v-if="threadDatas.userId == userConnected.id ">
-    <span><button type="button" v-on:click="modifyThread = !modifyThread">Modifier</button><button type="button" v-on:click="deleteThread">Supprimer</button></span>
+  <div class="Thread d-flex justify-content-center">
+    <div class="card border-primary mb-4">
+      <div class="card-header">{{ threadDatas.User.firstName + " " + threadDatas.User.lastName}}</div>
+      <div  class="card-body">
+        <div v-if="modifyThread">
+          <addThread :threadDatas="threadDatas" @my-event="updateThreads" />
+        </div>
+        <div v-else>
+          <h4 class="card-title">{{ threadDatas.title }}</h4>
+          <p class="card-text">{{ threadDatas.content }}</p>
+        </div>
+
+        <!-- buttons if owner -->
+        <div v-if="threadDatas.userId == userConnected.id ">
+        <span><button type="button" v-on:click="modifyThread = !modifyThread">Modifier</button><button type="button" v-on:click="deleteThread">Supprimer</button></span>
+        </div>
+
+        <!-- comments -->
+        <div v-if="threadDatas.Comments">
+          <div class="comments" v-for="comment in threadDatas.Comments" :key="comment.id">
+            <Comment :commentDatas="comment" :id="threadDatas.id" />
+          </div>
+        </div>
+
+        <!-- add comment -->
+        <addComment />
+      
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import addThread from '../components/addThread.vue'
+import Comment from './Comment.vue'
+import addComment from './addComment.vue'
 import { mapState } from "vuex"
 import Axios from 'axios'
-
-
 export default {
   name: 'Thread',
-  props: {
+  props: { 
     threadDatas: Object
   },
   data: function() {
@@ -35,7 +51,9 @@ export default {
       // console.log("props", this.threadDatas);
   },
   components: {
-    addThread
+    addThread,
+    Comment,
+    addComment
   },
   computed: {
         ...mapState({
@@ -54,7 +72,6 @@ export default {
         let config = {
           headers: { Authorization: "Bearer " + JSON.parse(localStorage.getItem('groupomaniaToken'))}
         }
-
         Axios.delete("/thread/" + this.threadDatas.id, config )
                 //  .then( response => response.json() )
               .then( res => {
